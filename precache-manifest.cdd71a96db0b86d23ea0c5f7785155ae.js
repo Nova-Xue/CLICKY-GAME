89a18880c1afcd27fa7da780d9605ba8fde41e93
Nova <xueshengyuan1118@hotmail.com>
2019-07-29 19:11:19 -0400

    Updates

---
+++ precache-manifest.cdd71a96db0b86d23ea0c5f7785155ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195f4cfe95698466fca9de40d5806457",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "052758e3a327d66a96fe",
+    "url": "/clicky-game/static/css/main.75144b4f.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "052758e3a327d66a96fe",
+    "url": "/clicky-game/static/js/main.5f3a86e3.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
